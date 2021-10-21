5cd987b8056e950bfb840079c27cf8ab062eb19a
xdhao <zxcqwe1wd@gmail.com>
2021-10-21 14:53:42 +0400

    исправил проход по лигам

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import xlsxwriter
 
 
 OUT_FILENAME = 'out.json'
-OUT_XLSX_FILENAME = 'out.xlsx'
+OUT_XLSX_FILENAME = 'from_wildstat.xlsx'
 f = open( "Страны и языки.json" , "rb" )
 jsonObject = json.load(f)
 f.close()
@@ -81,6 +81,7 @@ for div_countries in countries:
 
         soup1 = get_soup(c_url)
         leagues = soup1.find_all('div', class_='smn-left-g')
+        leagues.append(soup1.find('div', class_='smn-left-w'))
         l_items = []
         #for _, div_leagues in zip(range(2), leagues):
         for div_leagues in leagues:
@@ -93,89 +94,91 @@ for div_countries in countries:
                    
                 soup2 = get_soup(l_url)
                 league_name = soup2.find('h1').text
-                div_table = soup2.find('div', class_='content-rb')
-                teams = div_table.find_all('a')
-                t_items = []
-                for a_teams in teams:
-                    href_teams = a_teams.get('href')
-                    if '/club/' in href_teams:
-                        t_url = "http://wildstat.ru" + href_teams                             
-                        team_links.append(t_url)
-                        print("\tteam:"+str(xt)+t_url)
-                        xt = xt+1
+                if not 'Суперкубок' in league_name and 'Чемпионат' in league_name:
+                    league_name = soup2.find('h1').text + soup2.find('h2').text
+                    div_table = soup2.find('div', class_='content-rb')
+                    teams = div_table.find_all('a')
+                    t_items = []
+                    for a_teams in teams:
+                        href_teams = a_teams.get('href')
+                        if '/club/' in href_teams:
+                            t_url = "http://wildstat.ru" + href_teams                             
+                            team_links.append(t_url)
+                            print("\tteam:"+str(xt)+t_url)
+                            xt = xt+1
 
-                        soup3 = get_soup(t_url)
-                        club_name = soup3.find('h1').text + ' ' + soup3.find('h2').text
-                        div_stadium = soup3.find('div', id='middle_col')
-                        p_s  = div_stadium.find_all('p')
-                        for idx, p_st in enumerate(p_s):                        
-                            if p_st.find('b'):
-                                b_text = p_st.find('b').text
-                                if 'Стадион:' in b_text:
-                                    stadiums = p_s[idx].find('a')
-                                    s_items = []
-                                    if stadiums:
-                                        href_stadiums = stadiums.get('href')
-                                        if '/map_stadium/' in href_stadiums:
-                                            s_url = "http://wildstat.ru" + href_stadiums
-                                            stadium_links.append(s_url)
-                                            print("\tStadium:"+str(xs)+s_url)
-                                            xs = xs+1
+                            soup3 = get_soup(t_url)
+                            club_name = soup3.find('h1').text + ' ' + soup3.find('h2').text
+                            div_stadium = soup3.find('div', id='middle_col')
+                            p_s  = div_stadium.find_all('p')
+                            for idx, p_st in enumerate(p_s):                        
+                                if p_st.find('b'):
+                                    b_text = p_st.find('b').text
+                                    if 'Стадион:' in b_text:
+                                        stadiums = p_s[idx].find('a')
+                                        s_items = []
+                                        if stadiums:
+                                            href_stadiums = stadiums.get('href')
+                                            if '/map_stadium/' in href_stadiums:
+                                                s_url = "http://wildstat.ru" + href_stadiums
+                                                stadium_links.append(s_url)
+                                                print("\tStadium:"+str(xs)+s_url)
+                                                xs = xs+1
 
-                                            soup4 = get_soup(s_url)
-                                            stad_table = soup4.find('table')
-                                            s_td = stad_table.find_all('td')
-                                            for td in s_td:              
-                                                if td.find('h1'):
-                                                    namestad = td.find('h1').text                                              
-                                                if td.find('h2'):
-                                                    geoloc = td.find('h2').text.split(",")
-                                                    country_stad = geoloc[0]
-                                                    stad_city = geoloc[1]
-                                                if td.find('b'):
-                                                    barr = td.find_all('b')
-                                                    stad_open = barr[0].text
-                                                    if len(barr) > 1 :
-                                                        spect_count = barr[1].text
-                                            if namestad and country_stad and stad_city:
-                                                if stad_open and spect_count:
-                                                    s_item ={
-                                                        's_url': s_url,
-                                                        'namestad': namestad,
-                                                        'country_stad': country_stad,
-                                                        'stad_city': stad_city,
-                                                        'language': jsonObject[country_stad],
-                                                        'stad_open': stad_open, 
-                                                        'spect_count': spect_count,
-                                                    }
-                                                else:
-                                                    s_item ={
-                                                        's_url': s_url,
-                                                        'namestad': namestad,
-                                                        'country_stad': country_stad,
-                                                        'stad_city': stad_city,
-                                                        'language': jsonObject[country_stad],
-                                                        'stad_open': ' ', 
-                                                        'spect_count': ' ',
-                                                    }                                                    
-                                                s_items.append(s_item)
-                                            namestad = ' '
-                                            country_stad = ' '
-                                            stad_city = ' '
-                                            stad_open = ' '
-                                            spect_count = ' '
-                        t_item = {
-                            't_url': t_url,
-                            'stadiums': s_items,
-                            'club_name': club_name,
-                        }    
-                        t_items.append(t_item)               
-                l_item = {
-                    'l_url': l_url,
-                    'teams': t_items,
-                    'league_name': league_name,
-                }
-                l_items.append(l_item)
+                                                soup4 = get_soup(s_url)
+                                                stad_table = soup4.find('table')
+                                                s_td = stad_table.find_all('td')
+                                                for td in s_td:              
+                                                    if td.find('h1'):
+                                                        namestad = td.find('h1').text                                              
+                                                    if td.find('h2'):
+                                                        geoloc = td.find('h2').text.split(",")
+                                                        country_stad = geoloc[0]
+                                                        stad_city = geoloc[1]
+                                                    if td.find('b'):
+                                                        barr = td.find_all('b')
+                                                        stad_open = barr[0].text
+                                                        if len(barr) > 1 :
+                                                            spect_count = barr[1].text
+                                                if namestad and country_stad and stad_city:
+                                                    if stad_open and spect_count:
+                                                        s_item ={
+                                                            's_url': s_url,
+                                                            'namestad': namestad,
+                                                            'country_stad': country_stad,
+                                                            'stad_city': stad_city,
+                                                            'language': jsonObject[country_stad],
+                                                            'stad_open': stad_open, 
+                                                            'spect_count': spect_count,
+                                                        }
+                                                    else:
+                                                        s_item ={
+                                                            's_url': s_url,
+                                                            'namestad': namestad,
+                                                            'country_stad': country_stad,
+                                                            'stad_city': stad_city,
+                                                            'language': jsonObject[country_stad],
+                                                            'stad_open': ' ', 
+                                                            'spect_count': ' ',
+                                                        }                                                    
+                                                    s_items.append(s_item)
+                                                namestad = ' '
+                                                country_stad = ' '
+                                                stad_city = ' '
+                                                stad_open = ' '
+                                                spect_count = ' '
+                            t_item = {
+                                't_url': t_url,
+                                'stadiums': s_items,
+                                'club_name': club_name,
+                            }    
+                            t_items.append(t_item)               
+                    l_item = {
+                        'l_url': l_url,
+                        'teams': t_items,
+                        'league_name': league_name,
+                    }
+                    l_items.append(l_item)
         c_item = {
             'c_url': c_url,
             'leagues': l_items, 
